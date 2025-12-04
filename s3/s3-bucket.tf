0ca3ce864dd4821edb0d927efcc497071bd615e7
Felipe <alves.felipeteixeira@gmail.com>
2025-12-04 01:23:48 -0300

    update tc version

--- s3/s3-bucket.tf
+++ s3/s3-bucket.tf
@@ -1,12 +1,12 @@
 resource "aws_s3_bucket" "lambda_code" {
-  bucket = "lambda-code-tc3-g38-v1"
+  bucket = "lambda-code-tc4"
   tags = {
     Name = "Store lambda"
   }
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-tc3-g38-lanchonete-v1"
+  bucket = "terraform-state-tc4-lanchonete"
   tags = {
     Name = "Terraform Infra Directory"
   }
